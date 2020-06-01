41c684de1de81bd609bd1224470b205a2196b8bf
Kristine <ying.kristine.wang@gmail.com>
2020-06-01 19:08:26 -0400

    prepare to distribute package

---
+++ setup.py
@@ -0,0 +1,20 @@
+# setup.py file
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lambdata-kristineyw", # the name that you will install via pip
+    version="1.0",
+    author="Kristine Wang",
+    author_email="ywang03@email.com",
+    description="Kristine's First VS-Borne Repo!",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    license="MIT",
+    url="https://github.com/KristineYW/lambdata-kristineyw",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
