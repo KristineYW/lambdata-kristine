8775c849436282f1b5ca818cfc4ac45141c6cf76
Kristine <ying.kristine.wang@gmail.com>
2020-06-01 19:03:54 -0400

    need to push data for module

--- my_lambdata/hello.py
+++ my_lambdata/hello.py
@@ -1,7 +1,11 @@
 # Import enlarge function
 from my_lambdata.my_mod import enlarge
+from pandas import DataFrame
 
 print ("HELLO")
 
 # Invoke enlarge()
 print(enlarge(8))
+
+df = DataFrame({'state':['CT','CO','CA','TX']})
+print(df.head())
