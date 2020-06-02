af124d89cc38601cd5deb603005ad85fcc9321c0
Kristine <ying.kristine.wang@gmail.com>
2020-06-02 18:32:38 -0400

    added polos and autos

--- my_lambdata/hello.py
+++ my_lambdata/hello.py
@@ -5,7 +5,7 @@ from pandas import DataFrame
 from sklearn.model_selection import train_test_split
 
 
-df = DataFrame({'state':['CT','CO','CA','TX']})
+df = DataFrame({'state': ['CT', 'CO', 'CA', 'TX']})
 print(df.head())
 
 # Invoke split
--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -3,6 +3,7 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
+
 def enlarge(n):
     """
     Param n is a number
@@ -11,12 +12,19 @@ def enlarge(n):
     return n * 100
 
 # Split a dataframe into train, val, and test sets
+
+
 def df_split(df, target):
-    train1, val = train_test_split(df, train_size=0.85, test_size=0.15, stratify=df[target], random_state=42)
-    train2, test = train_test_split(train1, train_size=0.8, test_size=0.2, stratify=df[target], random_state=42)
+    train1, val = train_test_split(
+        df, train_size=0.85, test_size=0.15, stratify=df[target], random_state=42)
+    train2, test = train_test_split(
+        train1, train_size=0.8, test_size=0.2, stratify=df[target], random_state=42)
     return(train2, val, test)
 
-# Assign datetime datatype to a column and then split into year, month, and day columns
+# Assign datetime datatype to a column and then split into year, month,
+# and day columns
+
+
 def date_split(df, column):
     df[column] = pd.to_datetime(df[column], infer_datetime_format=True)
     new_df = df.copy()
@@ -27,14 +35,11 @@ def date_split(df, column):
     return(new_df)
 
 
-
-# this code breaks our ability to import enlarge from other files, if left in the global scope:
-
+# this code breaks our ability to import enlarge from other files, if left
+# in the global scope:
 if __name__ == "__main__":
-#     # only run the code below IF this script is invoked from the command-line
-#     # not if it is imported from another script
+    #     # only run the code below IF this script is invoked from the command-line
+    #     # not if it is imported from another script
     print("HELLO")
     y = int(input("Please choose a number: "))
     print(y, enlarge(y))
-
-
